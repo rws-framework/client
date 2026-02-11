55cd1c8971b5f1b116770258542db821703531b4
Papa Black <jakpawlik@gmail.com>
2026-02-11 17:29:08 +0100

    v2.23.4
    Changed files:
    - src/services/ApiService.ts

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -103,6 +103,10 @@ class ApiService extends TheService {
     {        
         return calls.get.bind(this)(`${this.config.get('backendUrl')}${this.config.get('apiPrefix') || ''}/api/rws/resource/${resourceName}`) as Promise<ITypesResponse>
     }
+
+    getBackendUrl(){
+        return backend.getBackendUrl.bind(this);
+    }
 }
 
 export default ApiService.getSingleton();
